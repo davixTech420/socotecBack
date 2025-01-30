2cb61c04acd2ad1dead9e1a0fd4588ec710513fc
Your Name <you@example.com>
2025-01-30 12:10:08 -0500

    PRUBEA CAmbio de contraseña

--- controllers/userController.js
+++ controllers/userController.js
@@ -145,7 +145,7 @@ exports.registerUser = async (req, res) => {
         role: "employee",
       });
       /*  res.status(200).json({ message: 'Usuario creado exitosamente' }); */
-      res.redirect("http://10.48.12.211:8081/singIn");
+      res.redirect("http://10.48.5.113:8081/singIn");
     } catch (error) {
       res.status(500).json({ error: 'Error al crear el empleado' });
     }
@@ -213,7 +213,7 @@ exports.emailPassword = async (req, res) => {
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '10m' });
 
-    const resetLink = `http://10.48.12.211:8081/(forgotPass)/${token}`;
+    const resetLink = `http://10.48.5.113:8081/forgotPass/${token}`;
     //autenticacion para enviar los gmails/
     const oAuth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
     oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
@@ -394,10 +394,8 @@ exports.deleteUser = async (req,res) => {
     }
     await user.destroy();
     res.status(200).json({message:"Usuario eliminado"});
-
   }catch(error){
     res.status(500).json(error);
-
   }
 }
 
